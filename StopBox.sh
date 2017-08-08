433df154364d6413037135344eff39636fb5c9f3
Enrico Bocchi <enrico.bocchi@cern.ch>
2017-08-08 10:43:55 +0200

    Adding confirmation prompt to StopBox.sh

--- StopBox.sh
+++ StopBox.sh
@@ -7,6 +7,21 @@ source etc/common.sh
 
 
 # ----- STOP ----- #
+echo ""
+echo "WARNING: This script will terminate the running services (i.e., EOS, CERNBox, SWAN) and remove Docker containers."
+read -r -p "Do you want to continue [y/N] " response
+case "$response" in
+    [yY])
+        echo "Ok."
+        ;;
+    *)
+        echo "Cannot continue. Exiting..."
+        echo ""
+        exit 1
+        ;;
+esac
+
+
 # Preliminary Checks
 echo ""
 echo "Preliminary checks..."
@@ -17,45 +32,10 @@ check_required_services_are_available
 #warn_about_interfence_eos_cvmfs
 
 # Removing Containers
-echo ""
-echo "Stopping and removing containers..."
 check_single_user_container_running "stop"
 stop_and_remove_containers
 cleanup_folders_for_fusemount
 
 # Removing docker network
 docker_network_remove $DOCKER_NETWORK_NAME
-
-# Preparation
-#docker_network
-#volumes_for_eos
-#volumes_for_ldap
-#volumes_for_cernbox
-#if check_override_certificates; then
-#	copy_override_certificates
-#fi
-#fetch_singleuser_notebook_image
-#fetch_system_component_images
-#check_to_have_all_images
-#check_ports_availability
-#set_the_locks
-
-
-# Run via Docker Compose
-#echo ""
-#echo "Run via docker-compose..."
-#docker-compose up -d
-
-#echo
-#echo "Configuring..."
-#while [[ -f "$HOST_FOLDER"/usercontrol-lock ]]
-#do
-#        sleep 5        
-#done
-
-#echo ""
-#echo "Done!"
-#echo "Access to log files: docker-compose logs -f"
-#echo "Or get them sorted in time: docker-compose logs -t | sort -t '|' -k +2d"
-#echo "--> Please source the uboxed/etc/common.sh file first! <--"
 
