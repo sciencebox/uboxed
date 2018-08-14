7b174394c1e31e6cea89ec109026f47eefc82b7a
Enrico Bocchi <enrico.bocchi@cern.ch>
2018-08-14 14:43:42 +0200

    Infer from env vars the connection params to LDAP

--- selftest.d/test_ldap.sh
+++ selftest.d/test_ldap.sh
@@ -2,29 +2,28 @@
 set -o errexit # bail out on all errors immediately
 set -x
 
+source /selftest.d/.env
+
 # http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
 CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 OUTPUT_DIR=$CWD"/ldap_results"
 LDAP_OUT=$OUTPUT_DIR"/ldap.log"
-LDAPS_OUT=$OUTPUT_DIR"/ldaps.log"
 PAM_OUT=$OUTPUT_DIR"/pam.log"
-
-LDAP_CLIENTS="cernbox eos-fuse eos-mgm eos-mq jupyterhub"
-
 mkdir -p $OUTPUT_DIR
+
+LDAP_CLIENTS="eos-mq eos-mgm eos-fuse cernbox jupyterhub"
 
 
 # Query LDAP server and contrast obtained results
 # 1.from LDAP container itself (check you have some users and generate groundtruth)
-docker exec ldap ldapsearch -x -H ldap://ldap -b dc=example,dc=org -D "cn=admin,dc=example,dc=org" -w admin > $LDAP_OUT
-docker exec ldap ldapsearch -x -H ldaps://ldap -b dc=example,dc=org -D "cn=admin,dc=example,dc=org" -w admin > $LDAPS_OUT
-diff $LDAP_OUT $LDAPS_OUT || exit 1
+docker exec ldap ldapsearch -x -H $LDAP_URI -b $LDAP_BASE_DN -D $LDAP_ADMIN_BIND_DN -w $LDAP_ADMIN_BIND_PASSWORD > $LDAP_OUT
 
 # 2.from other containers
 for i in $LDAP_CLIENTS;
 do
-	docker exec $i ldapsearch -x -H ldap://ldap -b dc=example,dc=org -D "cn=admin,dc=example,dc=org" -w admin > $LDAP_OUT.$i
-	diff $LDAP_OUT $LDAP_OUT.$i || exit 1
+  docker exec $i ldapsearch -x -H $LDAP_URI -b $LDAP_BASE_DN -D $LDAP_ADMIN_BIND_DN -w $LDAP_ADMIN_BIND_PASSWORD > $LDAP_OUT.$i
+  diff $LDAP_OUT $LDAP_OUT.$i || exit 1
 done
 
 
@@ -32,18 +31,18 @@ done
 USERLIST=`cat $LDAP_OUT | grep uid: | cut -d ' ' -f 2 | tr '\n' ' ' | head -n 10`
 for i in $LDAP_CLIENTS;
 do
-	# eos-mq does not have ldap access via NSS/PAM and is expectd to fail
-	if [ "$i" == "eos-mq" ];
-	then
-		only_one_user=`echo $USERLIST | cut -d ' ' -f 1`
-		docker exec $i id $only_one_user && echo "ERROR: eos-mq should fail (no nss/pam configured) but succeeded" && exit 1
-		continue
-	fi
+  # eos-mq does not have ldap access via NSS/PAM and is expectd to fail
+  if [ "$i" == "eos-mq" ];
+  then
+    only_one_user=`echo $USERLIST | cut -d ' ' -f 1`
+    docker exec $i id $only_one_user && echo "ERROR: eos-mq should fail (no nss/pam configured) but succeeded" && exit 1
+    continue
+  fi
 
-	# for all the othe containers, scan user list
-	for un in $USERLIST;
-	do
-		docker exec $i id $un >> $PAM_OUT.$i || exit 1
-	done
+  # for all the othe containers, scan user list
+  for un in $USERLIST;
+  do
+    docker exec $i id $un >> $PAM_OUT.$i || exit 1
+  done
 done
 
