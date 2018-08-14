43f6762dc5b208499343f11fbba729e811cef3eb
Enrico Bocchi <enrico.bocchi@cern.ch>
2018-08-14 12:35:38 +0200

    Separate tests for cvmfs and squid

--- selftest.d/test_cvmfs.sh
+++ selftest.d/test_cvmfs.sh
@@ -5,12 +5,6 @@
 set -o errexit # bail out on all errors immediately
 set -x
 
-# Check the squid proxy is running
-squid -k check || exit 1
-
-# Check the reachability of Stratum 1 servers
-SQUID_STRATUM1=`cat etc/squid/squid.conf | grep -v '^#' | grep 'acl cvmfs dst' | cut -d ' ' -f 4 | tr '\n' ' '`
-
 # Probe CVMFS endpoint
 cvmfs_config probe || exit 1
 
@@ -27,10 +21,7 @@ CVMFS_PROXY=`cat /etc/cvmfs/default.local | grep -v '^#' | grep CVMFS_HTTP_PROXY
 # Note: Used only in case of DIRECT connection to Stratum 1 without local squid proxy
 CVMFS_SERVER=`cat /etc/cvmfs/domain.d/cern.ch.conf | grep -v "^#" | grep CVMFS_SERVER_URL | cut -d '=' -f 2 | sed 's/http:\/\///g' | sed 's/\/cvmfs\/@fqrn@//g' | tr -d '"' | tr ';' ' '`
 
-# Check the reachability of Stratum 1 servers (destinations of local squid proxy)
-SQUID_STRATUM1=`cat etc/squid/squid.conf | grep -v '^#' | grep 'acl cvmfs dst' | cut -d ' ' -f 4 | tr '\n' ' '`
-
-for i in $CVMFS_PROXY $CVMFS_SERVER $SQUID_STRATUM1 ;
+for i in $CVMFS_PROXY $CVMFS_SERVER ;
 do
 	ping -c 5 -i 0.2 -w 5 $i || exit 0
 done
