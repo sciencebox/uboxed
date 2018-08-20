e1ba7ebae07e7d7bd15311dfd0a65c8e3e84bea7
Enrico Bocchi <enrico.bocchi@cern.ch>
2018-08-20 11:35:55 +0200

    Update docker-compose location also for Ubuntu

--- SetupInstall-Ubuntu.sh
+++ SetupInstall-Ubuntu.sh
@@ -20,8 +20,8 @@ install_software()
   rm -f /tmp/docker-ce.deb
 
   echo "Installing docker-compose..."
-  wget https://github.com/docker/compose/releases/download/"$DOCKERCOMPOSE_VERSION"/docker-compose-`uname -s`-`uname -m` -O /usr/local/bin/docker-compose
-  chmod +x /usr/local/bin/docker-compose
+  wget https://github.com/docker/compose/releases/download/"$DOCKERCOMPOSE_VERSION"/docker-compose-`uname -s`-`uname -m` -O /bin/docker-comnpose
+  chmod +x /bin/docker-compose
 
   echo "Starting docker daemon..."
   service docker start
