6b8f33589fad73bbb8174f2ccc30dfcb347a3d6b
Enrico Bocchi <enrico.bocchi@cern.ch>
2019-07-09 16:11:43 +0200

    selftest: Update script to test deployment

--- TestHost.sh
+++ TestHost.sh
@@ -1,25 +1,23 @@
 #! /bin/bash
 
 export RUN_FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"   # This is the folder from where this scripts runs
-
-
-echo "Setting up the tests..."
-echo "This might take some time as a specific Docker image is being built"
-echo "  Logfile: # less test-setup.log"
-
-{
-    set -o verbose
-    docker build -t selftest -f selftest.Dockerfile .
-    docker rm -f selftest
-    docker run -d -it --name selftest --network demonet --volume /var/run/docker.sock:/var/run/docker.sock:rw selftest
-    set +o verbose
-} > test-setup.log 2>&1
+export IMAGE_NAME='gitlab-registry.cern.ch/sciencebox/docker-images/selftest:latest'
 
 echo ""
-echo "Done with the Docker image."
+echo "Setting up the tests..."
+
+echo ""
+echo "Pulling Docker image..."
+docker pull gitlab-registry.cern.ch/sciencebox/docker-images/selftest
+docker rm -f selftest >/dev/null 2>&1
+
+echo ""
+echo "Starting the container..."
+docker rm -f selftest >/dev/null 2>&1
+docker run -d -it --name selftest --network demonet --volume /var/run/docker.sock:/var/run/docker.sock:rw $IMAGE_NAME
+docker cp .env selftest:/root/selftest.d/.env
 
 echo ""
 echo "Running all tests..."
-echo "  Logfile: # docker exec -it selftest less /selftest.d/test.log"
-
-docker exec -it selftest bash /selftest.d/run_all_tests.sh
+echo "  Logfile: # docker exec -it selftest less /root/selftest.d/test.log"
+docker exec -it selftest bash /root/selftest.d/run_all_tests.sh
